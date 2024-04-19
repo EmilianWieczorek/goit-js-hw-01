66cae99e7472afcf7dc2b6ce36720011d6a0be0d
EmilianWieczorek <155941604+EmilianWieczorek@users.noreply.github.com>
2024-04-19 13:56:17 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,6 @@
 "use strict";
 
 function getShippingMessage(country, price, deliveryFee) {
-  country = "Australia";
   const totalPrice = price * deliveryFee;
   console.log(`Shipping to ${country} will cost ${totalPrice} credits`);
 }
