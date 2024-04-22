796191c8154b7482cab3d04ca694e4faaa5d99d0
EmilianWieczorek <155941604+EmilianWieczorek@users.noreply.github.com>
2024-04-22 14:20:04 +0200

    a

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,18 @@
 "use strict";
 
 function getElementWidth(content, padding, border) {
-  parseFloat = content + padding + border;
+  const parseFloat = Number.parseFloat(content + padding + border);
   console.log(`${parseFloat}`);
 }
+
+//const content = 1;
+//const padding = 1;
+//const border = 1;
+//function getElementWidth(content, padding, border) {
+//console.log(content);
+//console.log(padding);
+//console.log(border);
+//const parseFloat = content + padding + border;
+
+//console.log(`${parseFloat}`);
+//}
